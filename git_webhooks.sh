6d569df95b03e9f62eb9f1e8403379fc56f59868
aTool <i@atool.org>
2015-08-28 22:44:57 +0800

    fix sh

--- git_webhooks.sh
+++ git_webhooks.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #git update
-cd /home/wwwroot/default/hash-text
+cd /home/wwwroot/default/hash_text
 
 git pull origin master
 
 cd /home/wwwroot/default/
 
-\cp -rf hash-text/* ./
+\cp -rf hash_text/* ./
 
 echo "git webhooks update success"
